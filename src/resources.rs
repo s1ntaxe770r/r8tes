c845a45285ed02d4bec286b90f255aae033e7c84
s1ntaxe770r <cyberdev01@protonmail.com>
2023-07-22 17:38:13 +0100

    sperate CRDS

---
+++ src/resources.rs
@@ -0,0 +1,18 @@
+
+use kube::CustomResource;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug, Clone, Serialize, Deserialize, CustomResource, JsonSchema,Default)]
+#[kube(
+    group = "operators.gopher.net",
+    version = "v1",
+    kind = "RateCache",
+    namespaced,
+    shortname = "rc",
+    
+)]
+pub struct RateCacheSpec {
+    
+}
