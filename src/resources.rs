630c1b2cd4dc6a8ae7ca787943889f519982fee9
s1ntaxe770r <cyberdev01@protonmail.com>
2023-07-23 22:49:15 +0100

    crd updoots

--- src/resources.rs
+++ src/resources.rs
@@ -1,18 +1,13 @@
-
 use kube::CustomResource;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-
-#[derive(Debug, Clone, Serialize, Deserialize, CustomResource, JsonSchema,Default)]
+#[derive(Debug, Clone, Serialize, Deserialize, CustomResource, JsonSchema, Default, Copy)]
 #[kube(
     group = "operators.gopher.net",
     version = "v1",
     kind = "RateCache",
     namespaced,
-    shortname = "rc",
-    
+    shortname = "rc"
 )]
-pub struct RateCacheSpec {
-    
-}
+pub struct RateCacheSpec {}
