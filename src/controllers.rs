196a667540aeff3c5233ca6db7c54f4363a8e85b
s1ntaxe770r <cyberdev01@protonmail.com>
2023-09-08 14:44:17 +0100

    resolve permission bug

--- src/controllers.rs
+++ src/controllers.rs
@@ -1,7 +1,18 @@
 use crate::resources::RateCache;
 use anyhow::Result;
-use k8s_openapi::api::apps::v1::Deployment;
-use kube::{client, runtime::controller::Action, Api, Client};
+use k8s_openapi::{
+    api::{
+        apps::v1::{Deployment, DeploymentSpec},
+        core::v1::{Container, ContainerPort, PodSpec, PodTemplateSpec},
+    },
+    apimachinery::pkg::apis::meta::v1::LabelSelector,
+};
+use kube::{
+    api::PostParams, client, core::ObjectMeta, runtime::controller::Action, Api, Client,
+    ResourceExt,
+};
+use serde_json::json;
+use std::collections::BTreeMap;
 
 use log::info;
 use std::sync::Arc;
@@ -22,6 +33,7 @@ pub async fn reconciler(g: Arc<RateCache>, _ctx: Arc<()>) -> Result<Action, Erro
     let deploy_name = Arc::clone(&g);
     info!("creating cache");
     create_redis_deployment(deploy_name.metadata.name.clone().unwrap(), client).await;
+
     Ok(Action::requeue(Duration::from_secs(300)))
 }
 /// an error handler that will be called when the reconciler fails with access to both the
@@ -31,45 +43,54 @@ pub fn error_policy(obj: Arc<RateCache>, _error: &Error, _ctx: Arc<()>) -> Actio
 }
 
 async fn create_redis_deployment(name: String, client: kube::Client) {
-    let deployment = Api::<Deployment>::all(client.clone());
-    let image_name = name + "-r8s";
+    let deployment: Api<Deployment> = Api::namespaced(client.clone(), "rates");
 
-    // create redis deployment with redis alpine image
-    let mut dep = Deployment::default();
-    // set deployment namespace
-    dep.metadata.namespace = Some("rates".to_string());
-    // set deployment name
-    dep.spec = Some(k8s_openapi::api::apps::v1::DeploymentSpec {
-        replicas: Some(1),
-        selector: k8s_openapi::apimachinery::pkg::apis::meta::v1::LabelSelector {
-            match_labels: Some(std::collections::BTreeMap::new()),
-            match_expressions: None,
+    let deployment_name = format!("{}-r8tes", name);
+
+    let dep: Deployment = Deployment {
+        metadata: ObjectMeta {
+            name: Some(deployment_name.to_owned()),
+            namespace: Some("rates".to_string()), // Set your desired namespace
+            ..ObjectMeta::default()
         },
-        template: k8s_openapi::api::core::v1::PodTemplateSpec {
-            metadata: Some(k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta {
-                labels: Some(std::collections::BTreeMap::new()),
-                ..Default::default()
-            }),
-            spec: Some(k8s_openapi::api::core::v1::PodSpec {
-                containers: vec![k8s_openapi::api::core::v1::Container {
-                    ports: Some(vec![k8s_openapi::api::core::v1::ContainerPort {
-                        container_port: 6379,
-                        protocol: Some("TCP".to_string()),
-                        ..Default::default()
-                    }]),
-                    name: image_name.clone().to_string(),
-                    image: Some("redis:alpine".to_string()),
-
-                    ..Default::default()
-                }],
-                ..Default::default()
-            }),
-        },
-        ..Default::default()
-    });
-
-    let dep = deployment.create(&Default::default(), &dep).await;
-
+        spec: Some(DeploymentSpec {
+            replicas: Some(1),
+            selector: LabelSelector {
+                match_labels: Some({
+                    let mut labels = BTreeMap::new();
+                    labels.insert("app".to_owned(), "my-app".to_owned()); // Match with the labels in the PodTemplateSpec
+                    labels
+                }),
+                ..LabelSelector::default()
+            },
+            template: PodTemplateSpec {
+                spec: Some(PodSpec {
+                    containers: vec![Container {
+                        name: name.to_owned(),
+                        image: Some("redis:alpine".to_owned()), // Redis Alpine image
+                        ports: Some(vec![ContainerPort {
+                            container_port: 6379, // Redis default port
+                            ..ContainerPort::default()
+                        }]),
+                        ..Container::default()
+                    }],
+                    ..PodSpec::default()
+                }),
+                metadata: Some(ObjectMeta {
+                    labels: Some({
+                        let mut labels = BTreeMap::new();
+                        labels.insert("app".to_owned(), "my-app".to_owned()); // Set the same labels as in spec.selector
+                        labels
+                    }),
+                    ..ObjectMeta::default()
+                }),
+                ..PodTemplateSpec::default()
+            },
+            ..DeploymentSpec::default()
+        }),
+        ..Deployment::default()
+    };
+    let dep = deployment.create(&PostParams::default(), &dep).await;
     match dep {
         Ok(o) => info!("created {:?}", o),
         Err(e) => info!("create failed: {}", e),
